74d34bb802f0b9c64e25978a341f03b97803b7ba
tuwq <aheadqiang@126.com>
2018-11-12 11:32:24 +0800

    mui基本结构

---
+++ mobile_MUI/config/config.js
@@ -0,0 +1,22 @@
+window.timConfig = {
+	// 底部选项卡
+	timFooterBarArray: [
+		{
+			pageId: "chatlist.html",
+			pageUrl: "pages/chatlist/chatlist.html"
+		},
+		{
+			pageId: "contact.html",
+			pageUrl: "pages/contact/contact.html"
+		},
+		{
+			pageId: "me.html",
+			pageUrl: "pages/me/me.html"
+		}
+	],
+	// 中间webview所需减少的距离(防止全屏)
+	timStyle: {
+		top: "44px",
+		bottom: "50px"
+	}
+}
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 ## 仿造实现TIM
 ## 技术栈
-	移动端: ReactNative
-	服务端: SpringBoot + Netty + RabbitMQ + MyBatis  
+	移动端: MUI
+	服务端: SpringBoot + Netty + MyBatis  
 	数据库: Mysql + Redis
