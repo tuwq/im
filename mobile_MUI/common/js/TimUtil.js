b9777ef21ede039e9e7b138ebd84a6160b9db23b
tuwq <aheadqiang@126.com>
2018-11-13 17:18:04 +0800

    登录跳转

--- mobile_MUI/common/js/TimUtil.js
+++ mobile_MUI/common/js/TimUtil.js
@@ -1,16 +1,48 @@
 document.write("<script language=javascript src='../../config/TimConfig.js'></script>");
 window.TimUtil = {
+	/**
+	 * toast弹窗
+	 * @param {Object} message 信息
+	 * @param {Object} type 图片类型
+	 */
 	showToast: function(message, type) {
 		plus.nativeUI.toast(message, {icon: "../../image/"+ type + ".png", verticalAlign: "bottom"})
 	},
+	/**
+	 * 缓存当前用户信息
+	 * @param {Object} user 用户对象
+	 */
+	setCacheNowUserInfo: function(user) {
+		var userInfoStr = JSON.stringify(user)
+		plus.storage.setItem("nowUserInfo", userInfoStr)
+	},
+	/**
+	 * 获取缓存用户信息
+	 */
+	getCacheNowUserInfo: function() {
+		return JSON.parse(plus.storage.getItem('nowUserInfo'))
+	},
+	/**
+	 * 清除缓存当前用户信息
+	 */
+	removeCacheNowUserInfo: function() {
+		plus.storage.removeItem('nowUserInfo')
+	},
+	/**
+	 * 封装ajax-post请求
+	 * @param {Object} url
+	 * @param {Object} data
+	 * @param {Object} success
+	 * @param {Object} fail
+	 */
 	postJson: function(url, data, success, fail) {
 		mui.ajax(window.TimConfig.serverUrl + url,{
 			data: data,
-			dataType:'json',//服务器返回json格式数据
-			type:'post',//HTTP请求类型
-			timeout:10000,//超时时间设置为10秒；
+			dataType:'json',
+			type:'post',
+			timeout:10000,
 			headers:{'Content-Type':'application/json'},	              
-			success:function(data){								
+			success:function(data){
 				success(data)
 			}
 		});
--- mobile_MUI/config/TimConfig.js
+++ mobile_MUI/config/TimConfig.js
@@ -1,19 +1,22 @@
 window.TimConfig = {
 	// 服务器地址
-	serverUrl: 'http://202cg57547.imwork.net:47424',
+	serverUrl: 'http://192.168.1.112:7000',
 	// 底部选项卡
 	timFooterBarArray: [
 		{
 			pageId: "chatlist.html",
-			pageUrl: "pages/Chatlist/chatlist.html"
+			pageUrl: "pages/Chatlist/chatlist.html",
+			title: '消息'
 		},
 		{
 			pageId: "contact.html",
-			pageUrl: "pages/Contact/contact.html"
+			pageUrl: "pages/Contact/contact.html",
+			title: '通讯录'
 		},
 		{
 			pageId: "me.html",
-			pageUrl: "pages/Me/me.html"
+			pageUrl: "pages/Me/me.html",
+			title: '我'
 		}
 	],
 	// 中间webview所需减少的距离(防止全屏)
