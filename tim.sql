f216197b82676b054e27bb3797e8fb278de56633
tuwq <aheadqiang@126.com>
2018-11-13 10:19:06 +0800

    修改表结构

--- tim.sql
+++ tim.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 80011
 File Encoding         : 65001
 
-Date: 2018-10-08 11:30:16
+Date: 2018-11-13 10:18:06
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,7 +20,7 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `friend_request`;
 CREATE TABLE `friend_request` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '好友请求关联id',
+  `id` varchar(50) NOT NULL COMMENT '好友请求关联id',
   `send_user_id` int(11) NOT NULL COMMENT '发送者id',
   `accept_user_id` int(11) NOT NULL COMMENT '接收者id',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
@@ -32,7 +32,7 @@ CREATE TABLE `friend_request` (
 -- ----------------------------
 DROP TABLE IF EXISTS `groups`;
 CREATE TABLE `groups` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '组id',
+  `id` varchar(50) NOT NULL COMMENT '组id',
   `group_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '组名称',
   `group_description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '组描述',
   `users_num` int(11) NOT NULL DEFAULT '0' COMMENT '组的用户数量',
@@ -45,7 +45,7 @@ CREATE TABLE `groups` (
 -- ----------------------------
 DROP TABLE IF EXISTS `group_chat_msg`;
 CREATE TABLE `group_chat_msg` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '组消息id',
+  `id` varchar(50) NOT NULL COMMENT '组消息id',
   `send_user_id` int(11) NOT NULL COMMENT '发送者id',
   `accept_group_id` int(11) NOT NULL COMMENT '接收组id',
   `msg` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '消息内容',
@@ -59,7 +59,7 @@ CREATE TABLE `group_chat_msg` (
 -- ----------------------------
 DROP TABLE IF EXISTS `group_request`;
 CREATE TABLE `group_request` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '组请求关联id',
+  `id` varchar(50) NOT NULL COMMENT '组请求关联id',
   `send_group_id` int(11) DEFAULT NULL COMMENT '发送组id',
   `accept_user_id` int(11) NOT NULL COMMENT '接收者id',
   `create_time` datetime NOT NULL COMMENT '创建时间',
@@ -71,7 +71,7 @@ CREATE TABLE `group_request` (
 -- ----------------------------
 DROP TABLE IF EXISTS `group_users`;
 CREATE TABLE `group_users` (
-  `id` int(11) NOT NULL COMMENT '组用户关联id',
+  `id` varchar(50) NOT NULL COMMENT '组用户关联id',
   `group_id` int(11) NOT NULL COMMENT '组id',
   `user_id` int(11) NOT NULL COMMENT '用户id',
   `create_time` datetime NOT NULL COMMENT '入群时间',
@@ -83,7 +83,7 @@ CREATE TABLE `group_users` (
 -- ----------------------------
 DROP TABLE IF EXISTS `my_friends`;
 CREATE TABLE `my_friends` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '好友关联id',
+  `id` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '好友关联id',
   `my_id` int(11) NOT NULL COMMENT '我的id',
   `my_friend_id` int(11) NOT NULL COMMENT '我好友的id',
   `create_time` datetime NOT NULL COMMENT '关联时间',
@@ -95,7 +95,7 @@ CREATE TABLE `my_friends` (
 -- ----------------------------
 DROP TABLE IF EXISTS `private_chat_msg`;
 CREATE TABLE `private_chat_msg` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '私人信息id',
+  `id` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '私人信息id',
   `send_user_id` int(11) NOT NULL COMMENT '发送者id',
   `accept_user_id` int(11) NOT NULL COMMENT '接收者id',
   `msg` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '消息内容',
@@ -109,14 +109,15 @@ CREATE TABLE `private_chat_msg` (
 -- ----------------------------
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '用户id',
-  `account_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '账号(qq号)',
+  `id` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户id',
+  `qq_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '账号(qq号)',
   `nickname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '昵称',
   `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码',
   `face_image` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '头像',
   `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '描述,签名',
-  `phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '手机号',
+  `telephone` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '手机号',
   `qrCode` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '个人二维码',
+  `app_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '手机唯一标识',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建事件',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -126,7 +127,7 @@ CREATE TABLE `users` (
 -- ----------------------------
 DROP TABLE IF EXISTS `user_message_sign_flag`;
 CREATE TABLE `user_message_sign_flag` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '消息可靠id',
+  `id` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '消息可靠id',
   `user_id` int(11) NOT NULL COMMENT '发送者id',
   `msg_id` int(11) NOT NULL COMMENT '消息id',
   `sign_flag` int(11) NOT NULL COMMENT '是否签收',
